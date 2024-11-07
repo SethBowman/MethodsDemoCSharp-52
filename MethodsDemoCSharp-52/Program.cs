7e8c1cf4fc61a7a0c40c0f852e81f033e6660bb2
Seth Bowman <brandonsethbowman@gmail.com>
2024-11-07 15:33:34 -0600

    demo completed

---
+++ MethodsDemoCSharp-52/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MethodsDemoCSharp_52;
+
+class Program
+{
+    //Create a method here
+    
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        // string userInput = Console.ReadLine();
+        
+        //Call the method (using the method)
+        GreetPerson("Seth");
+        GreetPerson("Logan");
+
+        int sum = AddTwoNumbers(73974, 2321);
+        Console.WriteLine(sum);
+    }
+    
+    //Or create a method here
+    //Method definition (creating a method)
+    //Pascal casing for methods
+    public static void GreetPerson(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    public static int AddTwoNumbers(int numOne, int numTwo)
+    {
+        //int sum = numOne + numTwo;
+
+        return numOne + numTwo;
+    }
+}
